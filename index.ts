4524021949b07aff1627c18ce9875b324575acb5
임인혜 <enae@im-inhyeui-MacBookPro.local>
2026-01-13 22:27:52 +0900

    cors설정 추가

--- index.ts
+++ index.ts
@@ -10,13 +10,24 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+    'https://enaeble.co.kr', // 메인 도메인
+    'https://portfolio2026.pages.dev', // Cloudflare 기본 주소
+    'http://localhost:3000' // 로컬 테스트용
+];
+
 app.use(cors({
-    origin: [
-        'https://enaeble.co.kr', // 메인 도메인
-        'https://portfolio2026.pages.dev', // Cloudflare 기본 주소
-        'http://localhost:3000' // 로컬 테스트용
-    ],
-    credentials: true // 쿠키 등을 주고받는다면 필수
+    origin(origin, callback) {
+        // 서버 → 서버 요청 (origin 없음)
+        if (!origin) return callback(null, true);
+    
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+    
+        callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
 }));
 
 const httpServer = http.createServer(app);
