82b90d5f757b38a198ca444b22fb07fd3ff029aa
임인혜 <enae@im-inhyeui-MacBookPro.local>
2026-01-17 03:41:01 +0900

    contact 파일 수정

--- views/src/api/contact.ts
+++ views/src/api/contact.ts
@@ -4,16 +4,16 @@ interface FORMDATA {
     captchaToken?: string;
 };
 
-await new Promise<void>((resolve) => {
-    window.grecaptcha.ready(() => resolve());
-});
-
-const token = await window.grecaptcha.execute(
-    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!,
-    { action: 'contact' }
-);
-
 export const contactMe = async(formState: FORMDATA) => {
+    await new Promise<void>((resolve) => {
+        window.grecaptcha.ready(() => resolve());
+    });
+    
+    const token = await window.grecaptcha.execute(
+        process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!,
+        { action: 'contact' }
+    );
+    
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact`, {
             method: 'POST',
